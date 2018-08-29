239d317e6da103ababddd13872047ea272761791
Dieter Haude <42463588+dhaude@users.noreply.github.com>
2018-08-29 10:15:56 +0200

    Anpassung an den ELV Bewegungssensor

--- Bewegungsmelder_esp8266.ino
+++ Bewegungsmelder_esp8266.ino
@@ -1,19 +1,8 @@
 /*
- Basic ESP8266 MQTT example
-
- This sketch demonstrates the capabilities of the pubsub library in combination
- with the ESP8266 board/library.
-
- It connects to an MQTT server then:
-  - publishes "hello world" to the topic "outTopic" every two seconds
-  - subscribes to the topic "inTopic", printing out any messages
-    it receives. NB - it assumes the received payloads are strings not binary
-  - If the first character of the topic "inTopic" is an 1, switch ON the ESP Led,
-    else switch it off
+ Bewegungsmelder ESP8266 MQTT
 
  It will reconnect to the server if the connection is lost using a blocking
- reconnect function. See the 'mqtt_reconnect_nonblocking' example for how to
- achieve the same result without blocking the main loop.
+ reconnect function. 
 
 */
 
@@ -22,11 +11,7 @@
 #include <PubSubClient.h>
 
 // Update these with values suitable for your network.
-/*
-const char* ssid = "HAUDE_NET";
-const char* password = "6408636750237785";
-const char* mqtt_server = "192.168.0.5";
-*/
+
 const char* ssid = "Makerspace-Wiesbaden01";
 const char* password = "goonline";
 const char* mqtt_server = "192.168.1.252";
@@ -37,12 +22,9 @@ PubSubClient client(espClient);
 long lastMsg = 0;
 char msg[50];
 int value = 0;
-int adc;
 byte a;
 byte ahis;
-byte b;
-byte bhis;
-byte c;
+
 void setup_wifi() { 
   // config static IP
   IPAddress ip(192, 168, 1, 202); // where xx is the desired IP Address
@@ -81,16 +63,6 @@ void callback(char* topic, byte* payload, unsigned int length) {
     Serial.print((char)payload[i]);
   }
   Serial.println();
-
-  // Switch on the LED if an 1 was received as first character
-  if ((char)payload[0] == '1') {
-    digitalWrite(BUILTIN_LED, LOW);   // Turn the LED on (Note that LOW is the voltage level
-    // but actually the LED is on; this is because
-    // it is acive low on the ESP-01)
-  } else {
-    digitalWrite(BUILTIN_LED, HIGH);  // Turn the LED off by making the voltage HIGH
-  }
-
 }
 
 void reconnect() {
@@ -120,11 +92,7 @@ void reconnect() {
 void setup() {
   pinMode(D4, OUTPUT);     // Initialize the BUILTIN_LED pin as an output
   pinMode(D2, INPUT);
-  pinMode(D5, INPUT);
-  pinMode(D0, OUTPUT);     // Initialize the Movement LED as output
   pinMode(D1, INPUT_PULLUP);     // manual activation button
-
-  digitalWrite(D0, false);  // Turn the LED off
       
   Serial.begin(115200);
   setup_wifi();
@@ -145,15 +113,11 @@ void loop() {
     ++value;
     
     a = digitalRead(D2);
-    b = digitalRead(D5) * 2; 
   
-  if(a != ahis || b != bhis ) {
-    ahis = a; bhis = b;
-    c = a || b;
+  if(a != ahis) {
+    ahis = a;
 
-    // digitalWrite(D4, !c);  // Turn the LED on/off
-    digitalWrite(D0, c);  // Turn the  movement LED on/off
-    snprintf (msg, 50, "%d", a+b);
+    snprintf (msg, 50, "%d", a);
     Serial.print("Publish message: ");
     Serial.println(msg);
     client.publish("stat/sensor_1/bewegung", msg, true);   
